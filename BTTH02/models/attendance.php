69af70f8413071aab7baa965825ec405e40975d2
NguyenMinhKhang2203 <nguyenminhkhang22032002@gmail.com>
2023-05-22 13:57:34 +0700

    tùy chỉnh và đảy dữ liệu điểm danh

--- BTTH02/models/attendance.php
+++ BTTH02/models/attendance.php
@@ -1,5 +1,9 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+?>
 
 <?php
+
 // Kết nối đến cơ sở dữ liệu
 $servername = "localhost";
 $username = "root";
@@ -11,60 +15,70 @@ if ($conn->connect_error) {
     die("Kết nối không thành công: " . $conn->connect_error);
 }
 
-// Truy vấn dữ liệu từ bảng `attendance`
-$sql = "SELECT * FROM attendance";
-$result = $conn->query($sql);
+// Xử lý dữ liệu khi form được gửi
+$successMessage = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $AttendanceTime = date("Y-m-d H:i:s"); // Lấy thời gian hiện tại
+    $AttendanceStatus = $_POST["status"];
+    $StudentID = $_POST["student_id"];
+    $ClassID = $_POST["class_id"];
 
+    // Tạo câu lệnh INSERT INTO để chèn dữ liệu vào bảng
+    $sql = "INSERT INTO attendance (AttendanceTime, AttendanceStatus, StudentID, ClassID) VALUES ( '$AttendanceTime', '$AttendanceStatus', '$StudentID', '$ClassID')";
+
+    if ($conn->query($sql) === true) {
+        $successMessage = "Điểm danh thành công!";
+    } else {
+        echo "Lỗi: " . $sql . "<br>" . $conn->error;
+    }
+  }
 
 // Đóng kết nối
 $conn->close();
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <head>
   <title>Form Điểm Danh</title>
-  <link rel="stylesheet" type="text/css" href="diemdanh.css">  
-  <script>
-    window.addEventListener('DOMContentLoaded', function() {
-      var timeInput = document.getElementById('time');
-      var currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-      timeInput.value = currentTime;
-    });
-  </script>
+   <link rel="stylesheet" type="text/css" href="diemdanh.css">   
+  <?php if ($successMessage): ?>
+      <div class="success-message"><?php echo $successMessage; ?></div>
+  <?php endif; ?>
 </head>
 <body>
   <div class="container">
     <h2>Điểm Danh</h2>
-    <form method="POST" action="process.php">
+   
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       <div class="form-group">
         <label for="time">Thời gian điểm danh:</label>
-        <input type="text" id="time" name="time" value="YYYY-MM-DD HH:MM:SS" disabled>
+        <input type="text" id="time" name="time" value="<?php echo date('Y-m-d H:i:s'); ?>" disabled>
       </div>
 
       <div class="form-group">
         <label>Trạng thái điểm danh:</label>
-        <input type="radio" id="present" name="status" value="present" for="present">Có mặt
-        <input type="radio" id="late" name="status" value="late" for="late">muộn
-        <input type="radio" id="absent" name="status" value="absent" for="absent">Vắng mặt
+        <input type="radio" id="present" name="status" value="Có mặt"for="present">Có mặt
+        <input type="radio" id="late" name="status" value="Muộn" for="late">Muộn
+        <input type="radio" id="absent" name="status" value="Vắng mặt" for="absent">Vắng mặt
       </div>
 
       <div class="form-group">
-        <label for="student-id">ID Sinh viên:</label>
-        <input type="text" id="student-id" name="student_id">
+        <label for="student_id">ID Sinh viên:</label>
+        <input type="text" id="student_id" name="student_id">
       </div>
 
       <div class="form-group">
-        <label for="class-id">ID Lớp:</label>
-        <input type="text" id="class-id" name="class_id">
+        <label for="class_id">ID Lớp:</label>
+        <input type="text" id="class_id" name="class_id">
       </div>
 
       <div class="form-group">
         <button type="submit">Gửi</button>
-      </div>
+    </div>
     </form>
+    
+     
   </div>
 </body>
 </html>
